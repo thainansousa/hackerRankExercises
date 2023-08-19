f1bd00b4d3398ec88aed2d0e441c1d35c8388468
Thainan Sousa <thainansousa7@gmail.com>
2023-08-18 23:46:53 -0300

    My solution from exercise plusMinus

---
+++ Easy/1 - plusMinus/index.py
@@ -0,0 +1,30 @@
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+
+    qtdPositiveValues = 0
+
+    qtdNegativeValues = 0
+
+    qtdZeroValues = 0
+
+    for n in arr:
+
+        if n > 0:
+            qtdPositiveValues = qtdPositiveValues + 1
+
+        elif n < 0:
+            qtdNegativeValues = qtdNegativeValues + 1
+
+        else:
+            qtdZeroValues = qtdZeroValues + 1
+
+    print(f'{(qtdPositiveValues / len(arr)):.6f}')
+
+    print(f'{(qtdNegativeValues / len(arr)):.6f}')
+
+    print(f'{(qtdZeroValues / len(arr)):.6f}')
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <h3>In this repository will be published the my solutions for exercises praticed at the site of the HackerRank 🚀</h3>
 
-##### <p>Created by <a href="https://www.linkedin.com/in/thainan-sousa/">Thainan Sousa</a></p>
+<p>Created by <a href="https://www.linkedin.com/in/thainan-sousa/">Thainan Sousa</a></p>
+
+<strong>All rights reserverd - HackerRank</strong>
